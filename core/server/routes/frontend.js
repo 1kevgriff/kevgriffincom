d3c6de72b1e066e37f4ee501cc1581fc698188fe
azure provisioned user <kevin@griffGhostLinux.griffGhostLinux.b2.internal.cloudapp.net>
2014-07-04 17:16:39 +0000

    fixed www redirect

--- core/server/routes/frontend.js
+++ core/server/routes/frontend.js
@@ -7,6 +7,15 @@ var frontend    = require('../controllers/frontend'),
 module.exports = function (server) {
     var subdir = config().paths.subdir;
 
+    // ### www redirect
+    server.get('/*', function (req, res, next) {
+         if (req.headers.host.match(/^www/) !== null) {
+             res.redirect(301, 'http://' + req.headers.host.replace(/^www\./, '') + req.url);
+         } else {
+             next();
+         }
+     });
+
     // ### Frontend routes
     server.get('/rss/', frontend.rss);
     server.get('/rss/:page/', frontend.rss);
