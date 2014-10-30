9454d930d133c853695938395d1e1b2ec6c3ed09
Kevin Griffin <kevin@kevgriffin.com>
2014-10-30 13:45:37 -0400

    upgrade

--- core/server/xmlrpc.js
+++ core/server/xmlrpc.js
@@ -1,40 +1,58 @@
 var _               = require('lodash'),
-    config          = require('./config'),
-    errors          = require('./errorHandling'),
     http            = require('http'),
     xml             = require('xml'),
+    api             = require('./api'),
+    config          = require('./config'),
+    errors          = require('./errors'),
     pingList;
 
 // ToDo: Make this configurable
-pingList = [
-    { host: 'blogsearch.google.com', path: '/ping/RPC2' },
-    { host: 'rpc.pingomatic.com', path: '/' }
-];
+pingList = [{
+    host: 'blogsearch.google.com',
+    path: '/ping/RPC2'
+}, {
+    host: 'rpc.pingomatic.com',
+    path: '/'
+}];
 
 function ping(post) {
     var pingXML,
         title = post.title;
 
     // Only ping when in production and not a page
-    if (process.env.NODE_ENV !== 'production' || post.page) {
+    if (process.env.NODE_ENV !== 'production' || post.page || config.isPrivacyDisabled('useRpcPing')) {
+        return;
+    }
+
+    // Don't ping for the welcome to ghost post.
+    // This also handles the case where during ghost's first run
+    // models.init() inserts this post but permissions.init() hasn't
+    // (can't) run yet.
+    if (post.slug === 'welcome-to-ghost') {
         return;
     }
 
     // Need to require here because of circular dependency
-    return config.urlForPost(require('./api').settings, post, true).then(function (url) {
-
+    return config.urlForPost(api.settings, post, true).then(function (url) {
         // Build XML object.
         pingXML = xml({
-            methodCall: [
-                { methodName: 'weblogUpdate.ping' },
-                {
-                    params: [{
-                        param: [{ value: [{ string: title }]}],
-                    }, {
-                        param: [{ value: [{ string: url }]}],
+            methodCall: [{
+                methodName: 'weblogUpdate.ping'
+            }, {
+                params: [{
+                    param: [{
+                        value: [{
+                            string: title
+                        }]
                     }]
-                }
-            ]
+                }, {
+                    param: [{
+                        value: [{
+                            string: url
+                        }]
+                    }]
+                }]
+            }]
         }, {declaration: true});
 
         // Ping each of the defined services.
@@ -51,8 +69,8 @@ function ping(post) {
             req.on('error', function (error) {
                 errors.logError(
                     error,
-                    "Pinging services for updates on your blog failed, your blog will continue to function.",
-                    "If you get this error repeatedly, please seek help from https://ghost.org/forum."
+                    'Pinging services for updates on your blog failed, your blog will continue to function.',
+                    'If you get this error repeatedly, please seek help from https://ghost.org/forum.'
                 );
             });
             req.end();
